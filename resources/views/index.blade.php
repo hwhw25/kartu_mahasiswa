268c258e2da9740b057f863fd1aa790f30325662
Rizki Nur Alawi <rizkyalawy39@gmail.com>
2022-11-05 10:56:45 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Input Mahsiswa</title>
+</head>
+<body>
+  <form>
+    <label for="fname">First name:</label><br>
+    <input type="text" id="fname" name="fname"><br>
+    <label for="lname">Last name:</label><br>
+    <input type="text" id="lname" name="lname">
+  </form>
+</body>
+</html>
